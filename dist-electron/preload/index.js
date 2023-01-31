659f4afdb6978cb1e686aaec7b41fe1719a66a60
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2023-01-31 05:07:51 -0500

    remove dists

--- dist-electron/preload/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("electronAPI", {
-  selectFile: () => electron.ipcRenderer.invoke("selectFile"),
-  getCache: () => electron.ipcRenderer.invoke("getCache")
-});
-//# sourceMappingURL=index.js.map
