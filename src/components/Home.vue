3c6563017c8693637432a4a7d5a24e2896ca143c
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2023-01-31 19:15:32 -0500

    search with debounce

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,20 +6,44 @@ import ChannelTile from './ChannelTile.vue'
 import { Channel } from '../../shared/channel';
 
 interface IHomeState {
-  channels: Array<Channel>
+    channels: Array<Channel>,
+    searchValue: string
 }
 
 const electron: any = (window as any).electronAPI;
 const router = useRouter();
 const state: IHomeState = reactive({
-    channels: []
+    channels: [],
+    searchValue: ""
 });
+var elementsToShow = 12;
+
+function debounce(fn: Function, ms = 300) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+}
+
+function filterChannels() {
+    console.log("test");
+    state.channels = ChannelService.channels
+        .filter(y => y.name.toLowerCase().indexOf(state.searchValue.toLowerCase()) > -1)
+        .slice(0, elementsToShow);
+}
+
+function searchInput(event:any) {
+    state.searchValue = event.target.value;
+    debounce(() => filterChannels(), 500)();
+}
+
 onMounted(async () => {
     ChannelService.channels = await electron.getCache();
     if (ChannelService.channels.length == 0)
         router.replace("/setup");
     else {
-        state.channels = ChannelService.channels.slice(0,10);
+        state.channels = ChannelService.channels.slice(0, elementsToShow);
     }
 })
 </script>
@@ -29,12 +53,13 @@ onMounted(async () => {
         <div class="container" style="margin-top: 5rem;">
             <div class="row mb-5">
                 <div class="mx-auto col-xl-6 col-lg-6 col-md-8 col-12">
-                    <input type="text" style="width: 100%;" class="form-control" placeholder="Type to search...">
+                    <input :value="state.searchValue" @input="searchInput" type="text" style="width: 100%;"
+                        class="form-control" placeholder="Type to search...">
                 </div>
             </div>
 
             <div class="row gy-3">
-                <div v-for="channel in state.channels" class="col-xl-3 col-lg-4 col-md-4" >
+                <div v-for="channel in state.channels" class="col-xl-3 col-lg-4 col-md-4">
                     <ChannelTile :data="channel"></ChannelTile>
                 </div>
             </div>
