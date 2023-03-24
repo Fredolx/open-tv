dfc0dfb261eaf9c6b0bf19f4712418ff9788d733
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2023-03-24 00:30:40 -0400

    stream-keep-open

--- electron-open-tv/src/index.js
+++ electron-open-tv/src/index.js
@@ -258,7 +258,9 @@ async function playChannel(url, record) {
   let command = `${mpvPath} ${url} --fs`
   if (url.endsWith(".mp4") || url.endsWith(".mkv"))
     command += " --save-position-on-quit";
-  else if (record === true) {
+  else 
+    command += " --cache=no --hls-bitrate=max --keep-open=yes --keep-open-pause=no --stream-lavf-o=reconnect_streamed=1,reconnect_at_eof=1,reconnect_on_network_error=1";
+  if (record === true) {
     let recordingFilePath = join(settings?.recordingPath ?? defaultRecordingPath, getRecordingFileName());
     command += ` --stream-record="${recordingFilePath}"`
   }
