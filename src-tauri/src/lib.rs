0c9f65085aef078cf0d15db58bdcea46b1bcaf24
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2024-08-16 19:56:02 -0400

    move back run

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,20 @@
-
 use anyhow::Error;
 
+pub mod m3u;
+pub mod mpv;
+pub mod settings;
 pub mod sql;
 pub mod types;
 pub mod xtream;
-pub mod settings;
-pub mod m3u;
-pub mod mpv;
 
 fn print_error_stack(e: Error) {
     eprintln!("{:?}", e);
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use anyhow::Result;
 fn main() -> Result<()> {
-  open_tv_lib::sql::create_or_initialize_db()?;
-  run();
-  Ok(())
-}
-
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    open_tv_lib::sql::create_or_initialize_db()?;
+    open_tv_lib::run();
+    Ok(())
 }
