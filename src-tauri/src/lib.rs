127089ed36053f175119983722ee2fa7d5b9d2a0
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2024-08-17 01:42:24 -0400

    remove feature temporarily

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,7 +14,6 @@ fn print_error_stack(e: Error) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
