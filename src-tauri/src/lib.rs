032ba1b82035186af65240482ecf5f0833fa50db
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2025-01-08 10:48:30 -0700

    add single instance plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -28,6 +28,7 @@ pub mod xtream;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_clipboard_manager::init())
