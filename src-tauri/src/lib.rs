cb1fc8c1fcaeeda7233f8da65853f11e36127aac
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2025-01-08 10:54:06 -0700

    focus on existing instance when trying to launch 2nd instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -28,7 +28,12 @@ pub mod xtream;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_single_instance::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _, _| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_clipboard_manager::init())
