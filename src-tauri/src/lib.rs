7640a0159e1ab7e0efcba8037c5d52ad40d59888
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2025-01-13 19:46:38 -0700

    fix tray on linux

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -87,14 +87,22 @@ pub fn run() {
                 ..Default::default()
             }));
             let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit_i])?;
+            let show_i = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&show_i, &quit_i])?;
             TrayIconBuilder::new()
                 .menu(&menu)
                 .show_menu_on_left_click(false)
                 .on_menu_event(|app, event| match event.id.as_ref() {
                     "quit" => {
                         app.exit(0);
-                    }
+                    },
+                    "show" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.unminimize();
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    },
                     _ => {}
                 })
                 .on_tray_icon_event(|tray, event| match event {
