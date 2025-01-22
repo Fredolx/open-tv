0b0c72ccc017ec07727a40d38f38af08a2cdbf29
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2025-01-22 13:23:56 -0700

    add to settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,47 +1,58 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { debounceTime, distinctUntilChanged, fromEvent, map, Subscription } from 'rxjs';
-import { Settings } from '../models/settings';
-import { invoke } from '@tauri-apps/api/core';
-import { Router } from '@angular/router';
-import { open } from '@tauri-apps/plugin-dialog';
-import { Source } from '../models/source';
-import { MemoryService } from '../memory.service';
-import { ViewMode } from '../models/viewMode';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ConfirmDeleteModalComponent } from '../confirm-delete-modal/confirm-delete-modal.component';
+import { Component, ElementRef, HostListener, ViewChild } from "@angular/core";
+import { debounceTime, distinctUntilChanged, fromEvent, map, Subscription } from "rxjs";
+import { Settings } from "../models/settings";
+import { invoke } from "@tauri-apps/api/core";
+import { Router } from "@angular/router";
+import { open } from "@tauri-apps/plugin-dialog";
+import { Source } from "../models/source";
+import { MemoryService } from "../memory.service";
+import { ViewMode } from "../models/viewMode";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ConfirmDeleteModalComponent } from "../confirm-delete-modal/confirm-delete-modal.component";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrl: "./settings.component.css",
 })
 export class SettingsComponent {
   subscriptions: Subscription[] = [];
   settings: Settings = {
     use_stream_caching: true,
     default_view: ViewMode.All,
-    volume: 100
+    volume: 100,
+    restream_port: 3000,
   };
   viewModeEnum = ViewMode;
   sources: Source[] = [];
-  @ViewChild('mpvParams') mpvParams!: ElementRef;
+  @ViewChild("mpvParams") mpvParams!: ElementRef;
 
-  constructor(private router: Router, public memory: MemoryService, private nav: Router, private modal: NgbModal) { }
+  constructor(
+    private router: Router,
+    public memory: MemoryService,
+    private nav: Router,
+    private modal: NgbModal,
+  ) {}
 
   isInputFocused(): boolean {
     const activeElement = document.activeElement;
-    return activeElement instanceof HTMLInputElement || 
-           activeElement instanceof HTMLTextAreaElement || 
-           activeElement instanceof HTMLSelectElement;
+    return (
+      activeElement instanceof HTMLInputElement ||
+      activeElement instanceof HTMLTextAreaElement ||
+      activeElement instanceof HTMLSelectElement
+    );
   }
 
-  @HostListener('document:keydown', ['$event'])
+  @HostListener("document:keydown", ["$event"])
   onKeyDown(event: KeyboardEvent) {
-    if (event.key == "Escape" || event.key == "BrowserBack" || (event.key == "Backspace" && !this.isInputFocused())) {
+    if (
+      event.key == "Escape" ||
+      event.key == "BrowserBack" ||
+      (event.key == "Backspace" && !this.isInputFocused())
+    ) {
       if (this.memory.ModalRef) {
         this.memory.ModalRef.close("close");
-      }
-      else {
+      } else {
         this.goBack();
       }
       event.preventDefault();
@@ -54,19 +65,17 @@ export class SettingsComponent {
   }
 
   getSettings() {
-    invoke('get_settings').then(x => {
-      this.settings = x as Settings
-      if (this.settings.use_stream_caching == undefined)
-        this.settings.use_stream_caching = true;
-      if (this.settings.default_view == undefined)
-        this.settings.default_view = ViewMode.All;
-      if (this.settings.volume == undefined)
-        this.settings.volume = 100;
+    invoke("get_settings").then((x) => {
+      this.settings = x as Settings;
+      if (this.settings.use_stream_caching == undefined) this.settings.use_stream_caching = true;
+      if (this.settings.default_view == undefined) this.settings.default_view = ViewMode.All;
+      if (this.settings.volume == undefined) this.settings.volume = 100;
+      if (this.settings.restream_port == undefined) this.settings.restream_port = 3000;
     });
   }
 
   getSources() {
-    invoke('get_sources').then(x => {
+    invoke("get_sources").then((x) => {
       this.sources = x as Source[];
       if (this.sources.length == 0) {
         this.memory.AddingAdditionalSource = false;
@@ -77,18 +86,23 @@ export class SettingsComponent {
 
   ngAfterViewInit(): void {
     this.subscriptions.push(
-      fromEvent(this.mpvParams.nativeElement, 'keyup').pipe(
-        map((event: any) => {
-          return event.target.value;
-        })
-        , debounceTime(500)
-        , distinctUntilChanged()
-      ).subscribe(async () => {
-        await this.updateSettings();
-      }));
-    this.subscriptions.push(this.memory.RefreshSources.subscribe(_ => {
-      this.getSources();
-    }));
+      fromEvent(this.mpvParams.nativeElement, "keyup")
+        .pipe(
+          map((event: any) => {
+            return event.target.value;
+          }),
+          debounceTime(500),
+          distinctUntilChanged(),
+        )
+        .subscribe(async () => {
+          await this.updateSettings();
+        }),
+    );
+    this.subscriptions.push(
+      this.memory.RefreshSources.subscribe((_) => {
+        this.getSources();
+      }),
+    );
   }
 
   addSource() {
@@ -97,7 +111,9 @@ export class SettingsComponent {
   }
 
   async refreshAll() {
-    await this.memory.tryIPC("Successfully updated all sources", "Failed to refresh sources", () => invoke("refresh_all"));
+    await this.memory.tryIPC("Successfully updated all sources", "Failed to refresh sources", () =>
+      invoke("refresh_all"),
+    );
   }
 
   async goBack() {
@@ -106,8 +122,7 @@ export class SettingsComponent {
   }
 
   async updateSettings() {
-    if (this.settings.mpv_params)
-      this.settings.mpv_params = this.settings.mpv_params?.trim();
+    if (this.settings.mpv_params) this.settings.mpv_params = this.settings.mpv_params?.trim();
     await invoke("update_settings", { settings: this.settings });
   }
 
@@ -124,12 +139,16 @@ export class SettingsComponent {
   }
 
   async nuke() {
-    this.memory.ModalRef = this.modal.open(ConfirmDeleteModalComponent, { backdrop: 'static', size: 'xl', keyboard: false});
-    this.memory.ModalRef.result.then(_ => this.memory.ModalRef = undefined);
+    this.memory.ModalRef = this.modal.open(ConfirmDeleteModalComponent, {
+      backdrop: "static",
+      size: "xl",
+      keyboard: false,
+    });
+    this.memory.ModalRef.result.then((_) => (this.memory.ModalRef = undefined));
     this.memory.ModalRef.componentInstance.name = "ConfirmDeleteModal";
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(x => x.unsubscribe());
+    this.subscriptions.forEach((x) => x.unsubscribe());
   }
 }
