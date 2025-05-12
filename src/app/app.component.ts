3d66df1c5f432891a36e67094cca223c988160a4
Frederic Lachapelle <frederic.lachapelle7@gmail.com>
2025-05-12 13:34:00 -0700

    disable right click except for mat-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 import { DownloadService } from "./download.service";
 
 @Component({
@@ -10,6 +10,19 @@ export class AppComponent {
   title = "open-tv";
 
   constructor(private download: DownloadService) {}
+
+  @HostListener("document:contextmenu", ["$event"])
+  onRightClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (this.isInsideMenuTrigger(target)) {
+      return;
+    }
+    event.preventDefault();
+  }
+
+  private isInsideMenuTrigger(element: HTMLElement): boolean {
+    return !!element.closest("[mat-menu-trigger-for], [matMenuTriggerFor]");
+  }
 
   showDownloadManager() {
     return this.download.Downloads.size > 0;
