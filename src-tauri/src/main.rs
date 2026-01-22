4a5419f8900ef5596adce30956f050ae6861e722
Frederic Lachapelle <frederic.lachapelle7@gmail.com>
2026-01-22 10:33:39 -0500

    only apply DISABLE DMABUF for NVIDIA GPUs on Linux

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use anyhow::{Context, Result};
 use open_tv_lib::log;
+#[cfg(target_os = "linux")]
+use std::env;
+#[cfg(target_os = "linux")]
+use std::path::Path;
+
 fn main() -> Result<()> {
+    apply_gpu_fixes();
     _ = open_tv_lib::utils::check_nuke()
         .with_context(|| "Failed to delete db after nuke request")
         .inspect_err(|e| log::log(format!("{:?}", e)));
@@ -10,3 +16,14 @@ fn main() -> Result<()> {
     open_tv_lib::run();
     Ok(())
 }
+
+#[cfg(target_os = "linux")]
+pub fn apply_gpu_fixes() {
+    if Path::new("/proc/driver/nvidia").exists() {
+        eprintln!("NVIDIA GPU detected. Setting WEBKIT_DISABLE_DMABUF_RENDERER=1");
+        env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn apply_gpu_fixes() {}
