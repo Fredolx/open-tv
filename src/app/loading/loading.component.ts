8e08797fe79041550ffb9b3284cf021fc2dbae08
Beats <officebeats@gmail.com>
2026-01-31 15:15:36 -0600

    fix(frontend): initialize default value for loading component inputs

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -6,8 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: './loading.component.css',
 })
 export class LoadingComponent {
-  @Input()
-  center: boolean = false;
+  @Input() center: boolean = false;
   count = 0;
   texts: string[] = ['Consider donating to Beats TV', 'Loading your channels...'];
 
