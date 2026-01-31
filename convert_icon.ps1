a056d02b43983120016a59aae4afa59592f64a7e
Beats <officebeats@gmail.com>
2026-01-30 19:21:20 -0600

    docs: Final README and UI sync for v1.9.6

--- convert_icon.ps1
+++
@@ -1,4 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile("c:\Users\admin-beats\OneDrive\xo Vibe Coding xo\iptvnator\open-tv\temp_icon.png")
-$img.Save("c:\Users\admin-beats\OneDrive\xo Vibe Coding xo\iptvnator\open-tv\temp_icon_real.png", [System.Drawing.Imaging.ImageFormat]::Png)
-$img.Dispose()
